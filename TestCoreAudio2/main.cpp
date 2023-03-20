c462106be3d7d7e427e49a974547ced3e4c0e7a4
raidoum <raidoumamoru@gmail.com>
2023-03-20 09:26:48 +0900

    Initial Commit

---
+++ TestCoreAudio2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestCoreAudio2
+//
+//  Created by Shohei Kinuta on 2023/03/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
